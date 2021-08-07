294053d2f6ef7a707839246d4de930ea9195239b
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-07 09:18:59 -0300

    arquivos de simulaçao de retorno da api + syle.css basico + api.js, que simula o get da api

--- pwa-news-dio-api/src/index.mjs
+++ pwa-news-dio-api/src/index.mjs
@@ -1,29 +1,42 @@
-import express from 'express';
-import cors from 'cors';
-import economy from './economy.json';
-import technology from './technology.json';
-import world from './world.json';
-// import economy from './economy.json';
+import express from 'express'
+import cors from 'cors'
+import economy from './economy.json'
+import technology from './technology.json'
+import world from './world.json'
 
 const GROUP_NEWS = {
+  economy,
+  technology,
+  world
+}
+
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+app.use(cors())
+//  Cross-origin Resource Sharing
+//  Mecanismo utilizado pelos navegadores para compartilhar recursos entre diferentes origens
+
+app.get('/api', function (req, res) {
+  res.json({
     economy,
     technology,
     world
-}
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(cors());
-
-app.get('/api', function (req , res) {
-    res.json({
-        economy,
-        technology,
-        world
-    })
+  })
 })
 
-app.listen( PORT, function () {
-    console.log('O Pai tá ON!!!')
+app.get('/api/:subject', function (req, res) {
+  const { subject } = req.params
+  res.json(GROUP_NEWS[subject])
+})
+
+app.get('/api/:subject/:id', function (req, res) {
+  const { subject, id } = req.params
+  const allNews = GROUP_NEWS[subject]
+  const news = allNews.value.find(news => news.id === id)
+  res.json(news)
+})
+
+app.listen(PORT, function () {
+  console.log(`Server running on ${PORT} PORT`)
 })
