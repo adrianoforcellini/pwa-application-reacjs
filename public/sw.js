54bd963eaad5ccf35df776fce833db8f844c42c5
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-28 06:12:51 -0300

    Versao Completa - Todos os requisitos de uma PWA

--- public/sw.js
+++ public/sw.js
@@ -4,17 +4,16 @@ var urlsToCache = [
   '/index.html'
 ];
 
-window.addEventListener('install', event => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-window.addEventListener('activate', event => {
+self.addEventListener('activate', event => {
   var cacheWhitelist = ['pwa-task-manager'];
   event.waitUntil(
     caches.keys().then(cacheNames => {
@@ -29,12 +28,10 @@ window.addEventListener('activate', event => {
   );
 });
 
-window.addEventListener('fetch', function(event) {
-  console.log("fetch", event)
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.match(event.request).then(function (response) {
-        console.log("response", response)
         return response || fetch(event.request).then(function(response) {
           cache.put(event.request, response.clone());
           return response;
