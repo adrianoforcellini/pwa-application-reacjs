89fe434442de9818181ff93b70795e2de4cde708
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-28 02:01:39 -0300

    Adjust share-icon

--- src/container/Actions.jsx
+++ src/container/Actions.jsx
@@ -1,40 +1,41 @@
-import React, { memo } from 'react'
-import ShareIcon from '../images/share.png'
-import CopyIcon from '../images/copy.png'
+import React, { memo } from "react";
+import ShareIcon from "../images/share.png";
+import CopyIcon from "../images/copy.png";
 
-const navigatorHasShare = navigator.share
+const navigatorHasShare = navigator.share;
 
-const URL = 'https://pwa-wnews.netlify.app/'
+const URL = "https://pwa-wnews.netlify.app/";
 
 function Actions({ post, subject }) {
-  const { id, title } = post
+  const { id, title } = post;
 
   const shareInfo = () => {
     navigator.share({
       title: `PWA News - Breaking News - ${subject}`,
       text: title,
       url: URL,
-    })
-  }
+    });
+  };
 
   const copyInfo = () => {
-    navigator.clipboard.writeText(`${title} - *Learn more about in* ${URL}/${subject}/${id}`)
-  }
+    navigator.clipboard.writeText(
+      `${title} - *Learn more about in* ${URL}/${subject}/${id}`
+    );
+  };
 
   const renderAction = () => {
+    const action = navigatorHasShare ? shareInfo : copyInfo;
 
-    const action = navigatorHasShare ? shareInfo : copyInfo
+    const icon = navigatorHasShare ? ShareIcon : CopyIcon;
 
-    const icon = navigatorHasShare ? ShareIcon : CopyIcon
+    return (
+      <div className="share-icon">
+        <img alt="icon" src={icon} onClick={action} />
+      </div>
+    );
+  };
 
-    return <img alt="icon" width="40vw" height="40vw" src={icon} className="share-icon" onClick={action} />
-  }
-
-  return (
-    <div className="share">
-      {renderAction()}
-    </div>
-  )
+  return <div className="share">{renderAction()}</div>;
 }
 
-export default memo(Actions)
+export default memo(Actions);
